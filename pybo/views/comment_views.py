a94d1cc2f989f2106133351d410c7a2d809f4138
yoonsangkwak <yunsang08@naver.com>
2020-12-01 15:44:23 +0900

    update scroll refresh

--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -19,7 +19,7 @@ def create_question(question_id):
         comment = Comment(user=g.user, content=form.content.data, create_date=datetime.now(), question=question)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#comment_{}'.format(url_for('question.detail', question_id=question_id), comment.id))
     return render_template('comment/comment_form.html', form=form)
 
 
@@ -36,7 +36,7 @@ def modify_question(comment_id):
             form.populate_obj(comment)
             comment.modify_date = datetime.now()    # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=comment.question.id))
+            return redirect('{}#comment_{}'.format(url_for('question.detail', question_id=comment.question.id), comment.id))
     else:
         form = CommentForm(obj=comment)
     return render_template('comment/comment_form.html', form=form)
@@ -64,7 +64,7 @@ def create_answer(answer_id):
         comment = Comment(user=g.user, content=form.content.data, create_date=datetime.now(), answer=answer)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=answer.question.id))
+        return redirect('{}#comment_{}'.format(url_for('question.detail', question_id=answer.question.id), comment.id))
     return render_template('comment/comment_form.html', form=form)
 
 
@@ -81,7 +81,7 @@ def modify_answer(comment_id):
             form.populate_obj(comment)
             comment.modify_date = datetime.now()    # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=comment.answer.question.id))
+            return redirect('{}#comment_{}'.format(url_for('question.detail', question_id=comment.answer.question.id), comment.id))
     else:
         form = CommentForm(obj=comment)
     return render_template('comment/comment_form.html', form=form)
