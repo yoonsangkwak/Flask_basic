36656277d15be4cd66fa12f456b766d8cc10ae75
yoonsangkwak <yunsang08@naver.com>
2020-12-01 11:17:11 +0900

    upgrade login auth

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -6,6 +6,8 @@ from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
 
+import functools
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/signup/', methods=('GET', 'POST'))
@@ -55,3 +57,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
